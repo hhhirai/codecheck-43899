24a8bbdb3e69ca4c69c709279e993b7864848866
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-21 11:31:42 +0900

    あ

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,8 +1,5 @@
 package codecheck;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 
 
@@ -11,18 +8,36 @@ public class App {
 
 		int card_num = Integer.parseInt(args[0]);
 		int mp = Integer.parseInt(args[1]);
-		Map<Integer, Integer> in_map = new HashMap<Integer, Integer>();
+		int[] ws;  //コスト
+		int[] ps;  //攻撃
+		int j = 0;
 
 		for (int i = 2, l = args.length; i < l; i+=2) {
-
 			int point = Integer.parseInt(args[i]);
 			int cost = Integer.parseInt(args[i+1]);
 
-			in_map.put(point, cost);
+			ws[j] = cost;
+			ps[j] = point;
+
+			j++;
 		}
-		Object[] mapkey = in_map.keySet().toArray();
-        Arrays.sort(mapkey);
-		System.out.println(in_map);
+
+		int maxw = mp;  //コスト制限
+		int[][] dp = new int[ws.length + 1][maxw + 1];
+		int ret = 0;
+
+		for (int i = 0; i < ws.length; i++) {
+		    for (int ｋ = 0; ｋ <= maxw; ｋ++) {
+		        if (ws[i] <= ｋ) {
+		            dp[i + 1][ｋ] = Math.max(dp[i][ｋ], dp[i][ｋ - ws[i]] + ps[i]);
+		            ret = Math.max(ret, dp[i + 1][ｋ]);
+		        } else {
+		            dp[i + 1][ｋ] = dp[i][ｋ];
+		        }
+		    }
+		}
+
+		System.out.println(ret);
 
 	}
 
