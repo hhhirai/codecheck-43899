e0be361c2eff20e7c273973aea5b893cf76659be
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-21 10:59:55 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,29 @@
 package codecheck;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+		int card_num = Integer.parseInt(args[0]);
+		int mp = Integer.parseInt(args[1]);
+		Map<Integer, Integer> in_map = new HashMap<Integer, Integer>();
+
+		for (int i = 2, l = args.length; i < l; i+=2) {
+
+			int point = Integer.parseInt(args[i]);
+			int cost = Integer.parseInt(args[i+1]);
+
+			in_map.put(point, cost);
 		}
+		in_map.entrySet().stream()
+		.sorted(java.util.Collections.reverseOrder(java.util.Map.Entry.comparingByKey()));
+
+		System.out.println(in_map);
+
 	}
+
 }
